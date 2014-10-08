7527d16653c4bca6bf6fab3dc52489859aa10277
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-10-07 17:03:23 -0700

    Refactored initial solution * use if/else instead of individual returns (since they're alternatives) * remove cluttering puts

--- kata_02_bin_chop/bin_chop_01_iterative.rb
+++ kata_02_bin_chop/bin_chop_01_iterative.rb
@@ -1,11 +1,7 @@
 def chop ( target_int, int_array )  
-  puts "chopping #{int_array} to find #{target_int}"
-
-  mid_index = int_array.length / 2
   left_index = 0
   right_index = int_array.length - 1
-
-  return -1 if int_array[mid_index].nil? 
+  mid_index = int_array.length / 2
 
   until (right_index - left_index) <= 1 do
     if target_int <  int_array[mid_index]
@@ -13,14 +9,16 @@ def chop ( target_int, int_array )
     else
       left_index = mid_index
     end
-    puts "current left: #{left_index}, mid: #{mid_index}, right: #{right_index}"
-    sleep(1)
     mid_index = left_index + (right_index - left_index)/2
     return mid_index if int_array[mid_index] == target_int
   end
 
-  return left_index if int_array[left_index] == target_int 
-  return right_index if int_array[right_index] == target_int 
-  return -1
+  if int_array[left_index] == target_int 
+    left_index 
+  elsif int_array[right_index] == target_int 
+    right_index
+  else
+    -1
+  end
 end
 
