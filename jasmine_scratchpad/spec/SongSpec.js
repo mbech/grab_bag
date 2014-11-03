5f11a85fd0b5699b7fab28c6724a5852a47e2823
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-02 19:25:31 -0800

    Add failing song name spec

--- jasmine_scratchpad/spec/SongSpec.js
+++ jasmine_scratchpad/spec/SongSpec.js
@@ -1,3 +1,14 @@
 describe("Song", function() {
 
-});
+  var song;
+
+  beforeEach(function(){
+    song = new Song("Title");
+  });
+
+  it("should have a name", function(){
+
+    expect(song.getName()).toEqual("Title");
+  })
+
+})
