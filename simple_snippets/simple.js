7b40b8db39f81c85dc6ba5c8d587e953f6be45c0
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-03 15:56:03 -0800

    Swap to using regex

--- simple_snippets/simple.js
+++ simple_snippets/simple.js
@@ -3,8 +3,10 @@ var simple_snippets = simple_snippets || {};
 simple_snippets.isFirstCharUpper = function(string){
   console.log("string[0] is: " + string[0]);
   console.log("string[0].toUpperCase is: " + string[0].toUpperCase());
+  
+  var re = /^[A-Z]/;
 
-  return string[0] === string[0].toUpperCase(); 
+  return string.match(re) ? true : false; 
 }
 
 simple_snippets.runTests = function(){
