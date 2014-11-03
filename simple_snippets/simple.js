0c3cbadd0743b1b2991ef364d8c873363f910e9e
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-03 15:57:31 -0800

    Comment out logs

--- simple_snippets/simple.js
+++ simple_snippets/simple.js
@@ -1,8 +1,8 @@
 var simple_snippets = simple_snippets || {};
 
 simple_snippets.isFirstCharUpper = function(string){
-  console.log("string[0] is: " + string[0]);
-  console.log("string[0].toUpperCase is: " + string[0].toUpperCase());
+  //console.log("string[0] is: " + string[0]);
+  //console.log("string[0].toUpperCase is: " + string[0].toUpperCase());
   
   var re = /^[A-Z]/;
 
@@ -10,6 +10,7 @@ simple_snippets.isFirstCharUpper = function(string){
 }
 
 simple_snippets.runTests = function(){
+  console.log("Running 'isFirstCharUpper()'...");
   console.log(this.isFirstCharUpper("test") + " - should be false");
   console.log(this.isFirstCharUpper("Really") + " - should be true");
   console.log(this.isFirstCharUpper("42") + " - should be false");
