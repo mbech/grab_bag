5c4d2b4795fb8df46c3c8a3d695b07824b73f87a
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-03 16:55:18 -0800

    implement arraySum

--- simple_snippets/js/simple.js
+++ simple_snippets/js/simple.js
@@ -3,8 +3,15 @@ var simple_snippets = simple_snippets || {};
 simple_snippets.isFirstCharUpper = function(string){
   var re = /^[A-Z]/;
   return string.match(re) ? true : false; 
-}
+};
 
 simple_snippets.circleArea = function(radius){
   return (radius * radius) * Math.PI;
-}
+};
+
+simple_snippets.arraySum = function(array){
+  var sum = array.reduce(function(prev_val, current_val){
+    return prev_val + current_val; 
+  });
+  return sum;
+};
