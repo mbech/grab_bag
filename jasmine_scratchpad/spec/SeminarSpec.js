e6b786fee38130fede8f0c56420852f9563cae6a
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-13 21:02:54 -0800

    Add spec for Seminar price, failing

--- jasmine_scratchpad/spec/SeminarSpec.js
+++ jasmine_scratchpad/spec/SeminarSpec.js
@@ -3,12 +3,15 @@ describe("Seminar", function(){
   var seminar;
 
   beforeEach(function(){
-    seminar = Seminar.create({name: "Jasmine"});
+    seminar = Seminar.create({name: "Jasmine", price: 100});
   });
 
   it("should have a name", function(){
     expect(seminar.getName()).toEqual("Jasmine");
   })
 
+  it("should have a price", function(){
+    expect(seminar.getPrice()).toEqual("100");
+  })
 
 })
