f54cb1233173905161485a76f50742941b4487a7
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-03 12:31:53 -0800

    Fully functioning implementation (recursive)
    Added inner function in order to create closure of outer functions
    return array (and not overwrite it in each recursion).

--- js_element_by_class/element_by_class.js
+++ js_element_by_class/element_by_class.js
@@ -2,24 +2,25 @@
 
 var getElementsByClassName = function(element, className){
   var matchedElements = [];
-  var childNodes = element.children;
 
-  if(childNodes.length === 0){
-    console.log("no children of element:");
-    console.log(element);
-    return [];
-  } 
-  for(var i=0, len=childNodes.length; i < len; i++){
-    console.log("checking out children of:");
-    console.log(element);
-    console.log(childNodes[i]);
-
-    if (childNodes[i].className.split(" ").indexOf(className) > -1){
-      matchedElements.push(childNodes[i]);
-      getElementsByClassName(childNodes[i]);
+  var recursiveChildSearch = function(element, className){
+    var childNodes = element.children;
+    if(childNodes.length === 0){
+      return [];
+    } 
+    for(var i=0, len=childNodes.length; i < len; i++){
+      if (childNodes[i].className.split(" ").indexOf(className) > -1){
+        matchedElements.push(childNodes[i]);
+      }
+      if (childNodes[i].children.length > 0){
+        recursiveChildSearch(childNodes[i], className);
+      }
     }
   }
+  recursiveChildSearch(element, className);
+
   return matchedElements;
 }
 
 console.log(getElementsByClassName(document.body, "header"));
+console.log(getElementsByClassName(document.body, "inner"));
