368431e5bc6ab922c082a693af86f58311af3b7b
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-03 12:20:21 -0800

    Initial version, partially working, some bugs
    Gets first-level children, does not seem to be working recursively at
    the moment.

--- js_element_by_class/element_by_class.js
+++ js_element_by_class/element_by_class.js
@@ -1,1 +1,25 @@
-alert("working");
+//Implement the getElementsByClassName(element, className) function.
+
+var getElementsByClassName = function(element, className){
+  var matchedElements = [];
+  var childNodes = element.children;
+
+  if(childNodes.length === 0){
+    console.log("no children of element:");
+    console.log(element);
+    return [];
+  } 
+  for(var i=0, len=childNodes.length; i < len; i++){
+    console.log("checking out children of:");
+    console.log(element);
+    console.log(childNodes[i]);
+
+    if (childNodes[i].className.split(" ").indexOf(className) > -1){
+      matchedElements.push(childNodes[i]);
+      getElementsByClassName(childNodes[i]);
+    }
+  }
+  return matchedElements;
+}
+
+console.log(getElementsByClassName(document.body, "header"));
