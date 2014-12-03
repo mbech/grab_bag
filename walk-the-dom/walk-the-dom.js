ad4a33cfd35b675b4084607ed50529470c60b012
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-03 13:13:28 -0800

    Implement function to walk dom and call function

---
+++ walk-the-dom/walk-the-dom.js
@@ -0,0 +1,16 @@
+//Function to traverse all children of supplied element and apply function
+//supplied as a parameter
+var walkTheDOM = function(node, func){
+  func(node);
+  
+  var childNode = node.firstChild;
+
+  while ( childNode ){
+    walkTheDOM(childNode, func);
+    childNode = childNode.nextSibling;  
+  }
+}
+
+walkTheDOM(document.body, function(node){
+  console.log("On node name: " + node.tagName);
+});
