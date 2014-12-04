5bce6f0485d41d91cb87677676baef535a1f55b8
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-03 21:27:10 -0800

    Implement function to find dupliate in array

--- misc_js/array_manipulation.js
+++ misc_js/array_manipulation.js
@@ -1,12 +1,20 @@
 //Find second highest number in an integer array
-secondMax = function(array){
+secondMax = function(array) {
   var sorted = array.sort();
   var max = sorted[sorted.length - 1];
   return sorted[sorted.indexOf(max) - 1];
 }
 
 //In an array 1-100 exactly one number is duplicate how do you find it
-findDuplicate = function(array){
-
+findDuplicate = function(array) {
+  var hash = {}; 
+  var i = array.length;
+  while (i--) {
+    if (hash[array[i]]) {
+      return array[i];
+    } else {
+      hash[array[i]] = true; 
+    }
+  }
 }
 
