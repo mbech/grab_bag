8e094ea220684ef95b7f5160025d5ca12dc521b7
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-03 19:19:24 -0800

    Implement string permutation function (recursive)

---
+++ misc_js/string_manipulation.js
@@ -0,0 +1,31 @@
+//Write a method which will remove any given character from a String
+var removeChar = function(string, character){
+  var len = string.length;
+  var output = "";
+
+  for(var i = 0; i < len; i++){
+    if ( string[i] !== character ){
+      output += string[i];
+    }
+  }
+  return output;
+};
+
+
+//Print all permutation of String 
+var recursiveStringPermute = function(string, prefix){
+  if(typeof prefix === "undefined"){
+    prefix = "";
+  }
+  var len = string.length;
+  if (len === 1){
+    //recursion bottomed out
+    console.log(prefix + string);
+  } else {
+    for(var i = 0; i < len; i++){
+      var substring = string.slice(0, i) + string.slice(i+1);
+      recursiveStringPermute(substring, prefix + string[i])
+    }
+  }
+};
+
