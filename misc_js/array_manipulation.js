357845199b000c76a4079c0bdf354e8254b7f0e3
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-03 21:21:12 -0800

    Add function signature

--- misc_js/array_manipulation.js
+++ misc_js/array_manipulation.js
@@ -4,3 +4,9 @@ secondMax = function(array){
   var max = sorted[sorted.length - 1];
   return sorted[sorted.indexOf(max) - 1];
 }
+
+//In an array 1-100 exactly one number is duplicate how do you find it
+findDuplicate = function(array){
+
+}
+
