8253c5dad8e874375e48f4f4cb06053ee2584ff5
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-03 21:18:37 -0800

    Remove console logs

--- misc_js/array_manipulation.js
+++ misc_js/array_manipulation.js
@@ -2,7 +2,5 @@
 secondMax = function(array){
   var sorted = array.sort();
   var max = sorted[sorted.length - 1];
-  console.log(sorted);
-  console.log(max);
   return sorted[sorted.indexOf(max) - 1];
 }
