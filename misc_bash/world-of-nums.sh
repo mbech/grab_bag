a0071a3b9bf36d0d741dad2b0f5bb511253d7176
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-05 16:39:01 -0800

    Implement simple number operations

---
+++ misc_bash/world-of-nums.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Given two integers X and Y, find their sum, difference, product and quotient.
+read X
+read Y
+
+echo $(($X + $Y))
+echo $(($X - $Y))
+echo $(($X * $Y))
+echo $(($X / $Y))
+
