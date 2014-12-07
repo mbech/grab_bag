283afb87a0a66c95d4484adb2eb08330c7637d2f
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-06 19:04:58 -0800

    Create file, add problem statement as comment

---
+++ misc_js/linked_list/stack_implementation.js
@@ -0,0 +1,3 @@
+//Implement a stack in C using either a linked list or a dynamic array, 
+//and justify your decision. Design the interface to your stack to be complete, 
+//consistent, and easy to use.
