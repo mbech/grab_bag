f845976a3ac3325bfd417d0c21289e271e6a4fef
mbech <matt.b.bechtel@gmail.com>
2015-01-04 17:25:47 -0800

    Add two tests for secondMax function

--- misc_js/test/array_tests.js
+++ misc_js/test/array_tests.js
@@ -1,6 +1,13 @@
 (function() {
   module("Array functions");
-  QUnit.test( "example test", function( assert ) {
-    assert.ok( 1 === 1, "Passed!" );
+  QUnit.test( "secondMax", function( assert ) {
+    var result,
+        unsortedArray = [5,2,6,7,3,11,3,2],
+        sortedArray = [1,2,3,4,5,6];
+
+    result = secondMax(unsortedArray);
+    assert.equal(result, 7, "secondMax of unsorted array" );
+    result = secondMax(sortedArray);
+    assert.equal(result, 5, "secondMax of sorted array" );
   });
 }());
