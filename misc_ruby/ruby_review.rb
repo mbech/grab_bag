1b02462375124186dda859772de8b6215de8612a
mbech <matt.b.bechtel@gmail.com>
2015-02-10 19:44:29 -0800

    Ruby inheritance review

--- misc_ruby/ruby_review.rb
+++ misc_ruby/ruby_review.rb
@@ -10,3 +10,29 @@ end
 def char_count(str, char)
   str.downcase.split('').keep_if { |current| current == char }.length
 end
+
+def param_yield(num1, num2, &block)
+  puts num1
+  puts num2
+  puts block.call(num1, num2)
+end
+
+class Dog
+  def initialize(name, age)
+    @name = name;
+    @age = age;
+  end
+  def bark
+    puts "big bark!"
+  end
+end
+
+class Puppy < Dog
+  def yip
+    puts @name << " small bark!"
+  end
+
+  def bark
+    puts "too small for big bark..."
+  end
+end
